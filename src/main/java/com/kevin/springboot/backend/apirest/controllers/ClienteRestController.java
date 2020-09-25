1862b8246db305aade17dde4548415c7bb86e082
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-09-25 00:40:28 -0600

    create method

--- src/main/java/com/kevin/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/kevin/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -1,10 +1,14 @@
 package com.kevin.springboot.backend.apirest.controllers;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +27,16 @@ public class ClienteRestController {
 	public List<Cliente> index(){
 		return clienteService.findAll();
 	}
+	
+	@GetMapping("/clientes/{id}")
+	public Cliente show(@PathVariable Long id){
+		return clienteService.findById(id);
+	}
+	
+	@PostMapping("/clientes")
+	public Cliente create(@RequestBody Cliente cliente) {
+		return clienteService.save(cliente);
+	}
+
 
 }
