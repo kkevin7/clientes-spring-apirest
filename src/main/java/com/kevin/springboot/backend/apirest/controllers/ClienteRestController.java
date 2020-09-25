91e78c65224aa69d48bdced82a891f28f67139ed
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-09-25 00:48:24 -0600

    other reques methods

--- src/main/java/com/kevin/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/kevin/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -4,12 +4,16 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kevin.springboot.backend.apirest.models.entity.Cliente;
@@ -34,9 +38,25 @@ public class ClienteRestController {
 	}
 	
 	@PostMapping("/clientes")
+	@ResponseStatus(HttpStatus.CREATED)
 	public Cliente create(@RequestBody Cliente cliente) {
 		return clienteService.save(cliente);
 	}
 
+	@PutMapping("/clientes/{id}")
+	public Cliente update(@RequestBody Cliente cliente, @PathVariable Long id) {
+		Cliente clienteActual = clienteService.findById(id);
+		
+		clienteActual.setNombre(cliente.getNombre());
+		clienteActual.setApellido(cliente.getApellido());
+		clienteActual.setEmail(cliente.getEmail());
+		
+		return clienteService.save(clienteActual);
+	}
+	
+	@DeleteMapping("/clientes/{id}")
+	public void delete(@PathVariable Long id) {
+		clienteService.delete(id);
+	}
 
 }
