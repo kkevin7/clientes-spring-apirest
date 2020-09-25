7fd5b2e797ad3622198ce590c93dd4a65cd028f8
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-09-25 09:27:14 -0600

    DELETE request

--- src/main/java/com/kevin/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/kevin/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -108,8 +108,19 @@ public class ClienteRestController {
 	}
 	
 	@DeleteMapping("/clientes/{id}")
-	public void delete(@PathVariable Long id) {
-		clienteService.delete(id);
+	public ResponseEntity<?> delete(@PathVariable Long id) {
+		
+		Map<String, Object> response = new HashMap<>();
+		
+		try {
+			clienteService.delete(id);
+		} catch (DataAccessException e) {
+			response.put("mensaje", "Error al realizar la consulta en la base de datos");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		response.put("mensaje", "El cliente ha sido eliminado con éxito");
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
 	}
 
 }
